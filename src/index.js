96d53d74876f85b6249c38c47c2b21770332cab3
Bernard-VERA <bvmars@hotmail.com>
2024-03-10 17:53:00 +0100

    Ajout d'images et de pages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import {BrowserRouter} from "react-router-dom"
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// eslint-disable-next-line
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
